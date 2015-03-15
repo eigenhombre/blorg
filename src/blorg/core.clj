6fc91ed5e395b60b66a1b4b97ee70835e16a6102
John Jacobsen <john@mail.npxdesigns.com>
2015-03-15 18:10:25 -0500

    Create files in target directory...
    ... and update when the source .org files
    change.  Fixes #1.

--- src/blorg/core.clj
+++ src/blorg/core.clj
@@ -1,15 +1,21 @@
 (ns blorg.core
   (:require [blorg.util :refer [pluralize]]
             [blorg.watcher :refer [start-watcher]]
+            [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]))
 
 
-(defn announce-file-changes [files]
-  (->> files
-       count
-       (#(format "say %d %s changed" % (pluralize (count files) "file")))
+(defn say [s]
+  (->> s
+       (format "say %s")
        (#(clojure.string/split % #" "))
-       (apply sh))
+       (apply sh)))
+
+
+(defn announce-file-changes [files]
+  (future
+    (let [n (count files)]
+      (say (format "%d %s changed" n (pluralize n "file")))))
   files)
 
 
@@ -18,7 +24,9 @@
           (count files)
           (if (= (count files) 1) "" "s"))
   (let [max-print 5
-        filenames (map #(.getAbsolutePath %) files)
+        filenames (map  #(if (string? %)
+                           %
+                           (.getAbsolutePath %)) files)
         print-files (if (< max-print (count files))
                       (-> (take max-print filenames)
                           vec
@@ -29,49 +37,57 @@
     files))
 
 
+;; FIXME make configurable
+(def output-dir "/tmp/blorg")
+(def blog-dir "/Users/jacobsen/Dropbox/org/blog/src")
+
+
+(defn- stripext [s]
+  (.substring s 0 (.lastIndexOf s ".")))
+
+
+(defn- target-file-name [fname]
+  (str output-dir "/" (stripext (.getName (io/file fname))) ".html"))
+
+
 (defn handle-changed-files [files]
   (doseq [f files]
-    (println f (count (slurp f))))
+    (let [html-name (target-file-name f)
+          output-contents (slurp f)]
+      (println f "->" html-name)
+      (spit html-name output-contents)))
   files)
 
 
+(defn all-blog-files []
+  (->> blog-dir
+       io/file
+       .listFiles
+       (remove #(.startsWith (.getName %) "."))
+       (map #(.getAbsolutePath %))
+       (filter #(.endsWith % ".org"))))
+
+
 (defn watch-directories []
-  (start-watcher ["/Users/jacobsen/Dropbox/org"
-                  "/Users/jacobsen/Programming/Lisp/Clojure/blorg"]
+  (start-watcher [blog-dir]
                  (comp announce-file-changes
                        display-file-changes
                        handle-changed-files)))
 
 
+(defn wait-forever []
+  (while true (Thread/sleep 1000)))
 
-(defn wait-forever [] (while true (Thread/sleep 1000)))
+
+;;; REMOVE BEFORE LEIN OR JAR:
+;; (-> (all-blog-files)
+;;     display-file-changes
+;;     handle-changed-files)
 
 
 (defn -main [& _]
+  (-> output-dir
+      io/file
+      .mkdir)
   (watch-directories)
   (wait-forever))
-
-
-;; REPL conveniences:
-
-
-(defonce watch-atom (atom nil))
-
-
-(defn watcher-on []
-  (when-not @watch-atom
-    (reset! watch-atom (watch-directories))))
-
-(defn watcher-off []
-  (when @watch-atom
-    (future-cancel @watch-atom)
-    (reset! watch-atom nil)))
-
-
-(comment
-
-(watcher-on)
-
-(watcher-off)
-
-)
