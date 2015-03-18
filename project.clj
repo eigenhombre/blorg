eb3b72a3f5d855e57e02db2de4a0c504c5881402
John Jacobsen <jjacobsen@opinionlab.com>
2015-03-17 21:39:25 -0500

    First steps at Bootstrap.  Flesh out content pages more.

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [watchtower "0.1.1"]
-                 [hiccup "1.0.5"]]
+                 [hiccup "1.0.5"]
+                 [environ "1.0.0"]
+                 [clj-time "0.9.0"]]
   :main blorg.core)
--- src/blorg/core.clj
+++ src/blorg/core.clj
@@ -1,9 +1,13 @@
 (ns blorg.core
   (:require [blorg.util :refer [pluralize]]
             [blorg.watcher :refer [start-watcher]]
+            [clj-time.coerce :as tc]
+            [clj-time.format :as tfmt]
             [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]
-            [hiccup.core :refer [html]]))
+            [environ.core :refer [env]]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [html5 include-css include-js]]))
 
 
 (defn say [s]
@@ -40,7 +44,7 @@
 
 ;; FIXME make configurable
 (def output-dir "/tmp/blorg")
-(def blog-dir "/Users/jacobsen/Dropbox/org/blog/src")
+(def blog-dir (-> :home env (str "/Dropbox/org/blog/src")))
 
 
 (defn- stripext [s]
@@ -57,9 +61,6 @@
   (str output-dir "/" (stripext (.getName (io/file fname))) ".html"))
 
 
-(defn pre-ify [s] (format "<pre>%s</pre>" s))
-
-
 (defn all-org-files [& [filter-name-regex]]
   (->> blog-dir
        io/file
@@ -73,7 +74,34 @@
        reverse))
 
 
-(def all-blog-posts (partial all-org-files #"\d{4}-\d\d-\d\d-"))
+(def ^:private date-re #"\d{4}-\d\d-\d\d")
+(def ^:private formatter (tfmt/formatter "yyyy-MM-dd"))
+(def all-blog-posts (partial all-org-files date-re))
+
+
+(defn filename->timestr [filename]
+  (some->> filename
+           (re-find date-re)
+           (tfmt/parse formatter)))
+
+
+(defn filename->timestr [filename]
+  (->> filename
+       io/file
+       .lastModified
+       tc/from-long))
+
+
+(defn file-date-str [filename]
+  {:pre [(string? filename)]}
+  (or (filename->timestr filename)
+      (filename->timestr filename)))
+
+
+(defn date-str-from-file [filename]
+  (->> filename
+       file-date-str
+       (tfmt/unparse formatter)))
 
 
 (defn extract-title-from-contents [s]
@@ -83,21 +111,126 @@
 
 
 (defn make-links []
-  (html [:ul
-         (for [f (all-blog-posts)
-               :let [link (-> f target-file-name stripdir)
-                     extracted-title (-> f slurp extract-title-from-contents)
-                     title (if extracted-title
-                             extracted-title
-                             (stripdir f))]]
-           [:li [:a {:href link} title]])]))
+  (html
+   [:ul
+    (for [f (all-blog-posts)
+          :let [link (-> f target-file-name stripdir)
+                extracted-title (-> f slurp extract-title-from-contents)
+                date-str (date-str-from-file f)
+                title (if extracted-title
+                        extracted-title
+                        (stripdir f))]]
+      [:li
+       [:a {:href link} title]
+       [:span {:class "filedate"} date-str]])]))
+
+
+(defn navbar []
+  [:nav
+   {:class "navbar navbar-default"}
+   [:div
+    {:class "container-fluid"}
+    [:div
+     {:class "navbar-header"}
+     [:button
+      {:type "button",
+       :class "navbar-toggle collapsed",
+       :data-toggle "collapse",
+       :data-target "#bs-example-navbar-collapse-1"}
+      [:span {:class "sr-only"} "Toggle navigation"]
+      [:span {:class "icon-bar"}]
+      [:span {:class "icon-bar"}]
+      [:span {:class "icon-bar"}]]
+     [:a {:class "navbar-brand", :href "#"} "johnj.com"]]
+    [:div
+     {:class "collapse navbar-collapse",
+      :id "bs-example-navbar-collapse-1"}
+     [:ul
+      {:class "nav navbar-nav"}
+      [:li
+       {:class "active"}
+       [:a
+        {:href "#"}
+        "Home"
+        [:span {:class "sr-only"} "(current)"]]]
+      [:li
+       {:class "dropdown"}
+       [:a
+        {:href "#",
+         :class "dropdown-toggle",
+         :data-toggle "dropdown",
+         :role "button",
+         :aria-expanded "false"}
+        "Tags"
+        [:span {:class "caret"}]]
+       [:ul
+        {:class "dropdown-menu", :role "menu"}
+        [:li {} [:a {:href "#"} "Lisp"]]
+        [:li {} [:a {:href "#"} "Physics"]]
+        [:li {} [:a {:href "#"} "Python"]]
+        [:li {} [:a {:href "#"} "Clojure"]]
+        [:li {:class "divider"}]
+        [:li {} [:a {:href "#"} "Art"]]
+        [:li {:class "divider"}]
+        [:li {} [:a {:href "#"} "Writing"]]]]]
+     [:form
+      {:class "navbar-form navbar-left", :role "search"}
+      [:div
+       {:class "form-group"}
+       [:input
+        {:type "text", :class "form-control", :placeholder "Search"}]]
+      [:button {:type "submit", :class "btn btn-default"} "Submit"]]
+     [:ul
+      {:class "nav navbar-nav navbar-right"}
+      [:li {} [:a {:href "#"} "About"]]
+      [:li
+       {:class "dropdown"}
+       [:a
+        {:href "#",
+         :class "dropdown-toggle",
+         :data-toggle "dropdown",
+         :role "button",
+         :aria-expanded "false"}
+        "Tags"
+        [:span {:class "caret"}]]
+       [:ul
+        {:class "dropdown-menu", :role "menu"}
+        [:li {} [:a {:href "#"} "Lisp"]]
+        [:li {} [:a {:href "#"} "Physics"]]
+        [:li {} [:a {:href "#"} "Python"]]
+        [:li {} [:a {:href "#"} "Clojure"]]
+        [:li {:class "divider"}]
+        [:li {} [:a {:href "#"} "Art"]]
+        [:li {:class "divider"}]
+        [:li {} [:a {:href "#"} "Writing"]]]]]]]])
 
 
 (defn prepare-html [f is-index?]
-  (let [contents (-> f slurp pre-ify)]
-    (if-not is-index?
-      contents
-      (str contents (make-links)))))
+  (html5
+   {:lang "en"}
+   [:meta {:charset "utf-8"}]
+   [:meta {:http-equiv "X-UA-Compatible"
+           :content "IE=edge"}]
+   [:meta {:name "viewport"
+           :content "width=device-width, initial-scale=1"}]
+   [:head
+    (include-css (str "https://maxcdn.bootstrapcdn.com/bootstrap/"
+                      "3.3.4/css/bootstrap.min.css"))
+    (include-js (str "https://ajax.googleapis.com/ajax/libs/jquery/"
+                     "1.11.2/jquery.min.js"))
+    (include-js (str "https://maxcdn.bootstrapcdn.com/bootstrap/"
+                     "3.3.4/js/bootstrap.min.js"))]
+   [:body
+    (navbar)
+    (let [contents (->> f slurp (vector :pre) html)
+          title (-> f slurp extract-title-from-contents)
+          date-str (date-str-from-file f)]
+      [:div
+       [:h1 title]
+       [:span date-str]
+       (if-not is-index?
+         contents
+         (str contents (make-links)))])]))
 
 
 (defn handle-changed-files [files]
@@ -107,7 +240,6 @@
          (let [html-name (target-file-name f)
                is-index? (->> f io/file .getName (= "index.org"))
                output-contents (prepare-html f is-index?)]
-           (println (if is-index? (str "**** " f) f) "->" html-name)
            (spit html-name output-contents)))
        (catch Throwable t
          (println t)))
@@ -118,7 +250,8 @@
   (start-watcher [blog-dir]
                  (comp announce-file-changes
                        display-file-changes
-                       handle-changed-files)))
+                       handle-changed-files
+                       (partial map #(.getAbsolutePath %)))))
 
 
 (defn wait-forever []
@@ -135,6 +268,7 @@
 
 ;;; REMOVE BEFORE LEIN OR JAR:
 ;; (-> (all-org-files)
+;;     announce-file-changes
 ;;     display-file-changes
 ;;     handle-changed-files)
-;; (spit "/tmp/blorg/links.html" (make-links))
+;; :ok
